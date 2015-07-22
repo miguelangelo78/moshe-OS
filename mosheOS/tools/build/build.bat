dbe04cac14c7b9ebd7d340fc37c72df67341114d
Miguel <santosmiguel25@gmail.com>
2015-07-22 17:42:36 +0100

    Binary files moved to the proper folder

--- mosheOS/tools/build/build.bat
+++ mosheOS/tools/build/build.bat
@@ -5,9 +5,11 @@
 	@echo ===========
 
 :: SET VARIABLES:
-	@set original_dir=%~dp0
-	@cd ../../moshe-system
-	@set system_dir=%CD%
+	:: GO TO PROJECT ROOT FOLDER
+		@cd ../../
+	@set moshe_root=%CD%
+	@set build_dir=%CD%\tools\build
+	@set system_dir=%moshe_root%\moshe-system
 	
 :: LAUNCH CYGWIN:
 	@echo off
@@ -15,10 +17,10 @@
 	chdir C:\cygwin\bin
 
 :: EXECUTE SHELL SCRIPT:
-	bash --login -i "%original_dir%/build.sh" "%system_dir%" "%original_dir%"
+	bash --login -i "%build_dir%/build.sh" "%system_dir%" "%build_dir%" "%moshe_root%"
 
 :: RETURN TO ORIGINAL FOLDER:
-	chdir "%original_dir%"
+	chdir "%build_dir%"
 
 :: MOVE THE KERNEL TO THE FLOPPY
 	@move /Y moshe-kernel.bin A:\moshe-kernel.bin
--- mosheOS/tools/build/build.sh
+++ mosheOS/tools/build/build.sh
@@ -2,12 +2,13 @@
 
 # $1 = System files (where the kernel is)
 # $2 = Build files (where the batch and shell files are, including the linker)
+# $3 = Root folder (where all folders are accessible)
 
 # COMPILE KERNEL:
-	i586-elf-gcc -Werror -Wall -O -fstrength-reduce -fomit-frame-pointer -nostdinc -fno-builtin -I"$1\kernel\include" -c -o "$2\out\main.o" "$1\kernel\main.c" -fno-strict-aliasing -fno-common -fno-stack-protector;
+	i586-elf-gcc -Werror -Wall -O -fstrength-reduce -fomit-frame-pointer -nostdinc -fno-builtin -I"$1\kernel\include" -c -o "$3\bin\main.o" "$1\kernel\main.c" -fno-strict-aliasing -fno-common -fno-stack-protector;
 
 # ASSEMBLE KERNEL LOADER:
-	nasm -f elf "$1\kernel\loader.asm" -o "$2\out\loader.o";
+	nasm -f elf "$1\kernel\loader.asm" -o "$3\bin\loader.o";
 
 # LINK THE TWO TOGETHER:
-	i586-elf-ld -o "$2\moshe-kernel.bin" -T "$2\linker.ld" "$2\out\main.o" "$2\out\loader.o";
+	i586-elf-ld -o "$2\moshe-kernel.bin" -T "$2\linker.ld" "$3\bin\main.o" "$3\bin\loader.o";
